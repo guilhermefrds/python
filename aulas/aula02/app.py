7d289304c3e3ff092bc3d9fd07eb782bf64a053a
guilhermefrds <guilhermeferdev@gmail.com>
2023-08-04 15:35:51 -0300

    New exercise

---
+++ aulas/aula02/app.py
@@ -0,0 +1,8 @@
+nome="manu"
+idade="13"
+gosto_de="TikTok"
+
+print("Meu nome e Guilherme")
+print("Eu gosto de programacao")
+print("Eu tenho 21 anos")
+print("Guilherme gosta de programacao e tem 21 anos")
