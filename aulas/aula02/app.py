d5bf3b1ef138bc2d66ad2fbfd4f69fdaededd36a
guilhermefrds <guilhermeferdev@gmail.com>
2023-08-05 11:40:00 -0300

    delete

--- aulas/aula02/app.py
+++ aulas/aula02/app.py
@@ -2,7 +2,7 @@ nome="manu"
 idade="13"
 gosto_de="TikTok"
 
-print("Meu nome e Guilherme")
+print("Meu nome e manu")
 print("Eu gosto de programacao")
 print("Eu tenho 21 anos")
-print("Guilherme gosta de programacao e tem 21 anos")
+print("manu gosta de programacao e tem 21 anos")
